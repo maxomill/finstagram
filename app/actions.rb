2c8b156fdd6fabcdc7a67da897e2fb5aeb1d1c42
Maximillian <maxkrewiak@gmail.com>
2016-11-15 04:01:06 +0000

    end of day

--- app/actions.rb
+++ app/actions.rb
@@ -7,35 +7,35 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  post_shark = {
+  @post_shark = {
     username: "sharky_j",
-    avatar_url: "http;//naserca.com/images/sharky_j.jpg",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
     humanized_time_ago: humanized_time_ago(15),
     time_ago_in_minutes: 15,
-    like_count: 0,
+    like_count: 158,
     comment_count: 1,
     comments: [{
         username: "sharky_j", 
         text: "I EAT EVERYTHING!!!!"
     }]
   }
-  post_whale = {
+  @post_whale = {
       username: "kirk_whalum",
-      avatar_url: "http://naserca.com?images?kirk_whalum.jpg",
+      avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
       photo_url: "http://naserca.com/images/whale.jpg",
       humanized_time_ago: humanized_time_ago(65),
-      like_count: 0,
+      like_count: 89,
       comment_count: 1,
       comments: [{
           username: "kirk_whalum",
           text: "#weekendvibes"
       }]
   }
-  post_marlin = {
+  @post_marlin = {
       username: "marlin_peppa",
       avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-      photo_url: "http//naserca.com/images/marlin.jpg",
+      photo_url: "http://naserca.com/images/marlin.jpg",
       humanized_time_ago: humanized_time_ago(190),
       like_count: 1,
       comments: [{
@@ -43,5 +43,7 @@ get '/' do
           text: "lunchtime! ;)",
       }]
   }
-   [post_shark, post_whale, post_marlin].to_s
+   @posts = [@post_shark, @post_whale, @post_marlin]
+   erb(:index)
 end
+
